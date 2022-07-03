7c24b557c96f1869c40deb3c6e92c6c73abc3857
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-07-03 12:47:57 +0600

    change route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const data = require("./db.js");
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(data);
+const router = jsonServer.router();
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4050;
 
