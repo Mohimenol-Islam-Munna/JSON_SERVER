5f809df10b9fdfa32984b6fc9e7d0e09fac6c96f
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-07-03 12:33:02 +0600

    new setup for server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("server js");
+const data = require("./db.js");
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4050;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
