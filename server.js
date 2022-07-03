abc74684d559c6348918bdc39d5a6f1a05f8ef59
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-07-03 12:49:46 +0600

    api route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const data = require("./db.js");
+const api = require("./db.js");
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router();
+const router = jsonServer.router(api);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4050;
 
